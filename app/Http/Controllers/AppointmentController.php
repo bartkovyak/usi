5eb506a382f58b0fd84f1670e2ac19d45cab9de9
Vladyslav Bartkovyak <vbartkovyak@gmail.com>
2017-05-14 14:24:42 +0200

    methods for appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -8,5 +8,10 @@ use App\Http\Requests;
 
 class AppointmentController extends Controller
 {
-    
+    public function get() {
+        return response()->json(Appointment::find(request()->route("id")));
+    }
+    public function show() {
+        return response()->json(Appointment::all());
+    }
 }

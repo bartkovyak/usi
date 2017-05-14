45a4233029e16f2b71b095c4057099e4d7fdc724
Vladyslav Bartkovyak <vbartkovyak@gmail.com>
2017-05-14 14:56:16 +0200

    methods for doctor

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -8,7 +8,22 @@ use App\Http\Requests;
 
 class DoctorController extends Controller
 {
-    //
+    public function create(){
+        $doctor = new Doctor();
+        $doctor->fill(request()->json()->all());
+        $doctor->save();
+        return response()->json(['id' => $doctor->id]);
+    }
+    public function edit($id){
+        $doctor = Doctor::find($id);
+        $doctor->fill(request()->json()->all());
+        $doctor->save();
+        return response()->json($doctor);
+    }
+    public function delete($id){
+        Doctor::destroy($id);
+        return response()->json(['id' => $id]);
+    }
     public function show(){
         $doctors = Doctor::all();
         return response()->json($doctors);
@@ -23,5 +38,15 @@ class DoctorController extends Controller
         $doctor = Doctor::find($id);
         $appointments = $doctor->appointments;
         return response()->json($appointments);
+    }
+    public function getAppointment($doctor_id, $appointment_id){
+        $result = \App\Appointment::where([
+            ['DOCTOR_id', intval($doctor_id)],
+            ['id', intval($appointment_id)]
+            ])->get();
+        return response()->json($result[0]);
+    }
+    public function getForSpeciality($speciality_id){
+        return response()->json(Doctor::where('SPECIALITY_id', intval($speciality_id))->get());
     }
 }

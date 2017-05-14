b8b50a16d309cf1225629fe3a22aef3987bd3713
Vladyslav Bartkovyak <vbartkovyak@gmail.com>
2017-05-14 14:17:59 +0200

    methods for speciality

--- app/Http/Controllers/SpecialityController.php
+++ app/Http/Controllers/SpecialityController.php
@@ -8,5 +8,10 @@ use App\Http\Requests;
 
 class SpecialityController extends Controller
 {
-    
+    public function get() {
+        return response()->json(Speciality::find(request()->route("id")));
+    }
+    public function show() {
+        return response()->json(Speciality::all());
+    }
 }

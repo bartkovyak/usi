3ed6bba69b9075076d262878edf22eb7c9b8a201
Vladyslav Bartkovyak <vbartkovyak@gmail.com>
2017-05-14 07:38:04 -0300

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,30 @@
 <?php
 
-// Route::metoda('sciezka', ['uses' => 'Kontroler@metoda']);
-
 Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
 
-Route::get('doctor', array('as' => 'read_doctors', 'uses' => 'DoctorController@show'));
+// Doctor
+Route::post('doctor/create', 'DoctorController@create');
+Route::match(['post', 'put'], 'doctor/{id}/edit', 'DoctorController@edit');
+Route::match(['get', 'delete'], 'doctor/{id}/delete', 'DoctorController@delete');
 Route::get('doctor/{id}', array('as' => 'read_doctor', 'uses' => 'DoctorController@get'));
 Route::get('doctor/{id}/appointment', array('as' => 'read_doctor_appointments', 'uses' => 'DoctorController@getAppointments'));
+Route::get('doctor/{id}/appointment/{id}', 'DoctorController@getAppointment');
+Route::get('doctor', array('as' => 'read_doctors', 'uses' => 'DoctorController@show'));
+Route::get('doctor/speciality/{id}', 'DoctorController@getForSpeciality');
+
+// Patient
+Route::post('patient/create', 'PatientController@create');
+Route::match(['post', 'put'], 'patient/{id}/edit', 'PatientController@edit');
+Route::match(['get', 'delete'], 'patient/{id}/delete', 'PatientController@delete');
+Route::get('patient/{id}', 'PatientController@get');
+Route::get('patient/{id}/appointment', 'PatientController@getAppointments');
+Route::get('patient/{id}/appointment/{id}', 'PatientController@getAppointment');
+Route::get('patient', 'PatientController@show');
+
+// Appointment
+Route::get('appointment/{id}', 'AppointmentController@get');
+Route::get('appointment', 'AppointmentController@show');
+
+// Speciality
+Route::get('speciality/{id}', 'SpecialityController@get');
+Route::get('speciality', 'SpecialityController@show');
